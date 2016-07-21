363ecc018f8cf0d53722f21bf111c7b46f0996f6
Abdelrahman El Kinani <kinani95@outlook.com>
2016-07-21 14:24:12 +0200

    Add DoVision(), DoFeel()

--- TimelineMe/MediaAdmin.cs
+++ TimelineMe/MediaAdmin.cs
@@ -4,6 +4,7 @@
  * Do we need to migirate this class to MediaViewModel ? overkill?
   TODO: TagsSpacesSeperated was intended to be used within search.
 */
+using Microsoft.ProjectOxford.Vision;
 using Microsoft.ProjectOxford.Vision.Contract;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
+using System.IO;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Microsoft.ProjectOxford.Emotion;
 
 namespace TimelineMe
 {
@@ -20,18 +24,40 @@ namespace TimelineMe
         private StorageFile currentMedia;
         private AnalysisResult currentMediaCVAnalysis;
         public List<MediaAdmin> ProcessedMediaCollection = new List<MediaAdmin>();
-        public MediaAdmin() { }
-
-        public void AddImage(StorageFile media, AnalysisResult analysisResult)
+        public MediaAdmin()
         {
+            
+        }
+
+        private async Task<AnalysisResult> DoVision(StorageFile file)
+        {
+            VisionServiceClient visonClient = new VisionServiceClient(MyCredentials.OxfordVision);
+            VisualFeature[] visualFeatures = new VisualFeature[] { VisualFeature.Adult, VisualFeature.Description, VisualFeature.Tags };
+            var fileStream = await file.OpenAsync(FileAccessMode.Read);
+            return await visonClient.AnalyzeImageAsync(fileStream.AsStream(), visualFeatures);
+        }
+
+        private async Task<Emotion[]> DoFeel(StorageFile file)
+        {
+            EmotionServiceClient emotionClient = new EmotionServiceClient(MyCredentials.OxfordEmtoion);
+            var fileStream = await file.OpenAsync(FileAccessMode.Read);
+            return await emotionClient.RecognizeAsync(fileStream.AsStream());
+        }
+
+        public void AddMedia(StorageFile media, AnalysisResult analysisResult)
+        {
+            // Install DoFeel(), DoVision()
+            // Install ExtractFeature()
+            // For Performance we're going to ExtractFeature after capturing each image immediatly
             MediaAdmin newMedia = new MediaAdmin();
             newMedia.currentMedia = media;
             newMedia.currentMediaCVAnalysis = analysisResult;
             _mediaAdmin.ProcessedMediaCollection.Add(newMedia);
         }
 
-        public ProcessedFeatures ExtractFeatures(AnalysisResult rawResult)
+        public ProcessedFeatures ExtractFeatures(AnalysisResult rawResult, Emotion[] emotions)
         {
+            // TODO: Add Emotions extraction
             // TODO: Traverse .Categeroies IEnum (should we abandon this?!)
             ProcessedFeatures _processedFeatures = new ProcessedFeatures();
             _processedFeatures.adultScore = rawResult.Adult.AdultScore;
@@ -55,6 +81,7 @@ namespace TimelineMe
     }
     public struct ProcessedFeatures
     {
+        // Add emotions extractions
         public double adultScore;
         public double CaptionsSoloConf;
         public string CaptionSoloText;
