740118d78c266e98800a97aa04f9a0577566ef6e
Abdelrahman El Kinani <kinani95@outlook.com>
2016-07-22 21:36:06 +0200

    Prototype for MediaViewModel

--- TimelineMe/ViewModels/MediaViewModel.cs
+++ TimelineMe/ViewModels/MediaViewModel.cs
@@ -9,38 +9,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Capture;
+using Windows.Storage;
 
 namespace TimelineMe.ViewModels
 {
     public class MediaViewModel : ViewModelBase
     {
         private MediaAdmin mediaAdmin = new MediaAdmin();
-        
-        private bool _isLoading = false;
+        #region Properties    
+        private bool isLoading = false;
         public bool IsLoading
         {
             get
             {
-                return _isLoading;
+                return isLoading;
             }
             set
             {
-                _isLoading = value;
+                isLoading = value;
                 RaisePropertyChanged("IsLoading");
 
             }
             
         }
 
+        private string youLastTime;
 
+        public string YouLastTime
+        {
+            get { return youLastTime; }
+            set
+            {
+                youLastTime = value;
+                RaisePropertyChanged("YouLastTime");
+            }
+        }
+        #endregion
 
+        #region Commands
+        private RelayCommand openCamera;
+
+        public RelayCommand OpenCamera
+        {
+            get
+            {
+                if(openCamera == null)
+                {
+                    openCamera = new RelayCommand(async () =>
+                    {
+                        await OpenCameraUI();
+                    });
+                }
+                return openCamera;
+            }
+            set
+            {
+                openCamera = value;
+            }
+        }
+
+        #endregion
+
+        public async Task OpenCameraUI()
+        {
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            //captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            if (photo != null)
+            {
+
+            }
+        }
         public MediaViewModel()
         {
             
         }
-        private void AddMedia()
+        private void AddImage()
         {
 
         }
+
+        private void RemoveImage()
+        {
+
+        }
+
+        private void MergeSelectedImages()
+        {
+
+        }
+
+
     }
 }
