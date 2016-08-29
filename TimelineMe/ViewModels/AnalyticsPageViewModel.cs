912e65c1089e7cbc96f676a505ac6a5fb3565ac8
Abdelrahman El Kinani <kinani95@outlook.com>
2016-08-29 14:31:53 +0200

    Hook MediaAdmin with AnalyticsPage VM

--- TimelineMe/ViewModels/AnalyticsPageViewModel.cs
+++ TimelineMe/ViewModels/AnalyticsPageViewModel.cs
@@ -1,20 +1,75 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimelineMe.Common;
+using TimelineMe.Models;
 
 namespace TimelineMe.ViewModels
 {
     public class AnalyticsPageViewModel : ViewModelBase
     {
+        private MediaAdmin mediaAdmin;
+
         #region Properties
 
+        private ObservableCollection<Media> mediaCollection;
+        public ObservableCollection<Media> MediaCollection
+        {
+            get
+            {
+                return mediaCollection;
+            }
+            set
+            {
+                mediaCollection = value;
+                RaisePropertyChanged("MediaCollection");
+            }
+        }
+        private ObservableCollection<MediaGroup> mediaGroupCollection;
+        public ObservableCollection<MediaGroup> MediaGroupCollection
+        {
+            get
+            {
+                return mediaGroupCollection;
+            }
+            set
+            {
+                mediaGroupCollection = value;
+                RaisePropertyChanged("MediaGroupCollection");
+            }
+        }
         #endregion
 
         #region Commands
 
+        private RelayCommand analyticsPageLoaded;
+
+
+        public RelayCommand AnalyticsPageLoaded
+        {
+            get
+            {
+
+                analyticsPageLoaded = new RelayCommand(() =>
+                {
+                    mediaAdmin.Initialize();
+                    MediaCollection = new ObservableCollection<Media>(mediaAdmin.MediaList);
+                    MediaGroupCollection = new ObservableCollection<MediaGroup>(mediaAdmin.MediaGroupList);
+
+                });
+
+                return analyticsPageLoaded;
+            }
+            set
+            {
+                analyticsPageLoaded = value;
+            }
+        }
         #endregion
         public AnalyticsPageViewModel() { }
     }
