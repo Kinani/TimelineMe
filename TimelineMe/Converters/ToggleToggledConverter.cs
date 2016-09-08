a3e3bed20195d5d28ef5143627db842ad54eccf8
Abdelrahman El Kinani <kinani95@outlook.com>
2016-09-08 14:53:12 +0200

    Implement Settings Page

---
+++ TimelineMe/Converters/ToggleToggledConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace TimelineMe.Converters
+{
+    public class ToggleToggledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            ToggleSwitch tgleSw = value as ToggleSwitch;
+            if (tgleSw != null)
+            {
+                if (tgleSw.IsOn == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TimelineMe/ViewModels/SettingsPageViewModel.cs
+++ TimelineMe/ViewModels/SettingsPageViewModel.cs
@@ -1,20 +1,87 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimelineMe.Common;
+using Windows.UI.Xaml.Controls;
 
 namespace TimelineMe.ViewModels
 {
     public class SettingsPageViewModel : ViewModelBase
     {
         #region Properties
-
+        private bool isAlarmTimePickerOn = true;
+        public bool IsAlarmTimePickerOn
+        {
+            get
+            {
+                return isAlarmTimePickerOn;
+            }
+            set
+            {
+                isAlarmTimePickerOn = value;
+                RaisePropertyChanged("IsAlarmTimePickerOn");
+            }
+        }
         #endregion
 
         #region Commands
-
+        private RelayCommand<object> oxfordToggled;
+        public RelayCommand<object> OxfordToggled
+        {
+            get
+            {
+               oxfordToggled = new RelayCommand<object>(tgleValue =>
+               {
+                   TLMESettings.EnableOxford = (bool)tgleValue;
+               });
+                return oxfordToggled;
+            }
+            set
+            {
+                oxfordToggled = value;
+            }
+        }
+        private RelayCommand<object> toastToggled;
+        public RelayCommand<object> ToastToggled
+        {
+            get
+            {
+                toastToggled = new RelayCommand<object>(tgleValue =>
+                {
+                    TLMESettings.EnableOxford = (bool)tgleValue;
+                    if (TLMESettings.EnableOxford)
+                        IsAlarmTimePickerOn = true;
+                    else
+                        IsAlarmTimePickerOn = false;
+                });
+                return toastToggled;
+            }
+            set
+            {
+                toastToggled = value;
+            }
+        }
+        private RelayCommand<object> timePickerChanged;
+        public RelayCommand<object> TimePickerChanged
+        {
+            get
+            {
+                timePickerChanged = new RelayCommand<object>(timepicker =>
+                {
+                    TimePicker timePicker = timepicker as TimePicker;
+                    TLMESettings.ScheduledDueTime = DateTimeOffset.Parse(timePicker.Time.ToString());
+                });
+                return timePickerChanged;
+            }
+            set
+            {
+                timePickerChanged = value;
+            }
+        }
         #endregion
         public SettingsPageViewModel() { }
     }
